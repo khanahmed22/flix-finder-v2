3b2a292fb5dbb50d4f487793c3369ba87e3ef8c9
Muhammad Ahmed Khan <ahmed.khans.mk@gmail.com>
2025-08-04 16:58:42 +0500

    zustand added

---
+++ src/authStore/authStore.js
@@ -0,0 +1,53 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { supabase } from "../db/supabase";
+
+
+
+
+
+
+export const useAuth = create(
+  persist((set)=>{
+    set(
+      ()=>  ({
+      session: null,
+      isLoading: true,
+      isError: null,
+
+      fetchSession: async ()=>{
+        set({isLoading: true})
+        try{
+          const {data,error} = await supabase.auth.getSession()
+          if(error) throw error
+          set({session: data.session, isLoading: false})
+        }
+
+        catch(err){
+          set({isError: err.message, isLoading: false})
+        }
+      },
+    //  setSession: (session)=> set({session}),
+      signOut: async ()=> {
+        await supabase.auth.signOut()
+        set({session:null})
+      }
+    }),
+
+    
+    
+    ),
+     {
+    name: 'authSession',
+    partialize:(state) => ({ session: state.session }), // only persist session
+  }
+}
+   
+  )
+  
+ 
+)
+
+
+
+
