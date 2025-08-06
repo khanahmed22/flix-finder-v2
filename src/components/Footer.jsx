262b1f6a01cbdf42b9c2bc75c41f855678f55aec
Muhammad Ahmed Khan <ahmed.khans.mk@gmail.com>
2025-08-06 16:54:38 +0500

    robots txt and sitemap added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,23 @@ export const Footer = () => {
     <footer className="footer bg-gray-850 flex items-center justify-around max-md:flex-col max-md:items-start bg-base-200 text-base-content p-10">
       <nav>
         <h4 className="footer-title">Services</h4>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
+        <a href="#" className="link link-hover">Branding</a>
+        <a href="#" className="link link-hover">Design</a>
+        <a href="#" className="link link-hover">Marketing</a>
+        <a href="#" className="link link-hover">Advertisement</a>
       </nav>
       <nav>
         <h4 className="footer-title">Company</h4>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <a href="#" className="link link-hover">About us</a>
+        <a href="#" className="link link-hover">Contact</a>
+        <a href="#" className="link link-hover">Jobs</a>
+        <a href="#" className="link link-hover">Press kit</a>
       </nav>
       <nav>
         <h4 className="footer-title">Legal</h4>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <a href="#" className="link link-hover">Terms of use</a>
+        <a href="#" className="link link-hover">Privacy policy</a>
+        <a href="#"className="link link-hover">Cookie policy</a>
       </nav>
       <form>
         <h4 className="footer-title">Newsletter</h4>
